975d0125ee801b2a60d31d4c8e5b2aa134ce9f5d
brianpr-pr <brian23perezperez@gmail.com>
2025-12-06 19:37:06 +0000

    Added user seeder

--- database/migrations/README.md
+++
@@ -1,5 +0,0 @@
-List of things to do or improve:
-
-Work on the models and seeders to poblate database.
-
-Add product_category table to the database.
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,6 +25,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProductCategorySeeder::class,
             ProductSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
